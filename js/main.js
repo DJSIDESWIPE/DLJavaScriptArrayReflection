279b99bd2dacda5f0382369c19ca6319c47ab5cd
DJSIDESWIPE <djleverton@outlook.com>
2022-11-24 09:55:11 +0000

    added styling to the page

--- js/main.js
+++ js/main.js
@@ -1,40 +1,99 @@
 let emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 let num = 0;
-let img = $('<img id="RandomImage">');
+let randomimg = $('<img id="RandomImage">');
+let randomimages = [];
+let randomimageid;
+let savedimages;
+
 $(".submit-btn").on("click",function(e){
     e.preventDefault();
+    let $email = $("#Email");
+    let $emailfield = $email.val().trim();
     console.log("button pressed");
-    if(!emailReg.test()){}
+    if(!emailReg.test($emailfield) || $emailfield === ""){
+        console.log("invalid email");
+        $email.addClass("email-error");
+    } else{
+        $email.removeClass("email-error");
+        let foundemail = false;
+        if(!savedimages){
+            savedimages = [{
+                emailaddress: $emailfield,
+                images: [randomimages[randomimageid].download_url]
+            }];
+            console.log(`no previous data stored, first data stored is:${savedimages[0].emailaddress}  ${savedimages[0].images}`);
+        } else {
+            for(var i=0; i < savedimages.length;i++){
+                if(savedimages[i].emailaddress === $emailfield){
+                    savedimages[i].images.push(randomimages[randomimageid].download_url)
+                    console.log(savedimages[i].images);
+                    foundemail = true;
+                } else {
+  
+                }
+            }
+            if(!foundemail){
+                savedimages.push({emailaddress: $emailfield,images: [randomimages[randomimageid].download_url]});
+                console.log(`${savedimages[savedimages.length-1].emailaddress}  ${savedimages[savedimages.length-1].images}`);
+            }
+        }
+        displayRandomImage(randomimages.length);
+        displaySavedImages();
+    }
 });
 
+function displaySavedImages(){
+    let $displaydiv = $("#Display-Images");
+    let htmltobedisplayed = "";
+    $displaydiv.html("");
+    for(var x = 0;x < savedimages.length ;x++){
+        console.log(savedimages[x].emailaddress)
+        htmltobedisplayed +=`<div class="display-images"> <h2 class='saved-header'>${savedimages[x].emailaddress}</h2>`;
+        for(var y = 0;y < savedimages[x].images.length ;y++){
+            console.log(`displaying saved image: ${savedimages[x].images[y]}`);
+            htmltobedisplayed += `<img class="saved-images" src="${savedimages[x].images[y]}">`;
+        }
+        htmltobedisplayed += `</div>`;
+    }
+    $displaydiv.html(htmltobedisplayed);
+}
+
+$(".refresh-btn").on("click",function(){
+    displayRandomImage(randomimages.length);
+});
+
+function displayRandomImage(randomnumberlimit){
+    randomimageid = randomNumber(randomnumberlimit)
+    console.log(`displaying image ID:${randomimageid}`);
+    randomimg.attr('src', randomimages[randomimageid].download_url);
+    randomimg.appendTo('#img-div');
+}
 function randomNumber(maxnum){
     maxnum++;
     num = Math.random()*maxnum;
-    console.log(num);
     return parseInt(num);
 }
-function temp1(arr){
+
+function loadImages(){
+    getImages(1);
+    for(var i = 2; i<34;i++){
+        getImages(i);
+    }
     
-    img.attr('src', arr[randomNumber()].download_url);
-    img.appendTo('#img-div');
 }
-function loadImage(){
-    let images = [];
-    const url = "https://picsum.photos/v2/list?page=1";
+function getImages(pagenum){
+    let url = `https://picsum.photos/v2/list?page=${pagenum}`;
     fetch(url)
     .then(response => response.json())
     .then(data=>{
         for(var i=0;i<data.length;i++){
-            images.push(data[i]);
+            randomimages.push(data[i]);
         }
-        console.log(images.length);
-        img.attr('src', images[randomNumber(30)].download_url);
-        img.appendTo('#img-div');
+        if(pagenum === 1){
+            displayRandomImage(30);
+        }
     })
     .catch(err=>{
-        console.log(JSON.stringify(err, null, 2));
+        console.log("error occured");
     })
-}
-function getImages(){
-    
 }
